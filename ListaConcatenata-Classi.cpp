b0bd320972ec7b0cf6d5983cb526075cf09af2e0
Antonio Maulucci <antonio.maulucci@libero.it>
2017-12-20 13:04:13 +0100

    Add files via upload

---
+++ ListaConcatenata-Classi.cpp
@@ -0,0 +1,99 @@
+/*
+ * ALLOCAZIONE DINAMICA DELLA MEMORIA E UTILIZZO DELLE CLASSI
+ * (C) Antonio Maulucci 2017
+ * Language: C++
+ *
+ * Questo programma definisce la classe Node ovvero un nodo di una lista concatenata
+ * implementa una funzione che mediante l'allocazione dinamica inserisce una stringa in coda ad una lista concatenata
+ * implementa una funzione per stampare tutte le stringhe contenute nella lista concatenata
+ */
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+using namespace std;
+
+
+
+/*prototipi*/
+void add(char*);
+void print();
+
+
+
+
+class Node
+{
+    public:
+        char* str;
+        Node *link;
+};
+
+
+
+
+Node* head = NULL;
+
+
+
+
+
+void add(char* s)
+{
+    if (head == NULL)
+    {
+        head = (Node*) malloc( sizeof(Node) );
+        head -> str = (char*) malloc(strlen(s)+1);
+        strcpy(head->str, s);
+    }
+    else
+    {
+        Node* cur = head, prec;
+        
+        while(cur)
+        {
+            prec = *cur;
+            cur = cur -> link;
+        }
+        
+        Node* newNode = (Node*) malloc(sizeof(Node));
+        prec.link = newNode;
+        newNode->str = (char*) malloc(strlen(s)+1);
+        strcpy(newNode->str, s);
+        
+    } //end of else
+}
+
+
+
+void print()
+{
+    Node* cur = head;
+    
+    while (cur)
+    {
+        printf("\n%s", cur->str);
+        cur = cur->link;
+    }
+}
+
+
+
+
+int main(int argc, char* argv[])
+{
+    
+    char str[256] = "";
+    
+    printf("Inserisci una stringa: "); //from ansi c
+    scanf("%s", str); //from ansi c
+    
+    add(str);
+    
+    print();
+    
+    printf("\n\n\n");
+    
+    return 0;
+}
